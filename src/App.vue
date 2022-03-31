d0523202490c9eb5e68a3986714810f99ce486d6
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2022-03-31 23:29:16 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>Количество лайков: <span>{{ likes }}</span> </h3>
+        <button @click="countLike" class="btn btn-primary">Увеличить количество</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            likes: 0,
+        }
+    }, 
+    methods: {
+
+        countLike: function() {
+            return this.likes += 1;
+        }
+
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
